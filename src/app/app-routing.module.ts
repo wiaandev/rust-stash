ed0e8b5f3fac5b1020906d8825bf3c423855fd9a
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2023-02-28 14:14:59 +0200

    ADD: Added Crafting Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CraftingComponent } from './pages/crafting/crafting.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { StashComponent } from './pages/stash/stash.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path: "", component: LandingComponent},
   {path: "stash", component: StashComponent},
   {path: "signup", component: SignupComponent},
+  {path: "crafting", component: CraftingComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +10,8 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { StashComponent } from './pages/stash/stash.component';
 import { ButtonComponent } from './components/button/button.component';
 import { SignupComponent } from './pages/signup/signup.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuickFilterComponent } from './components/quick-filter/quick-filter.component';
-import { FormsModule } from '@angular/forms';
+import { CraftingComponent } from './pages/crafting/crafting.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FormsModule } from '@angular/forms';
     ButtonComponent,
     SignupComponent,
     QuickFilterComponent,
+    CraftingComponent,
   ],
   imports: [
     BrowserModule,
