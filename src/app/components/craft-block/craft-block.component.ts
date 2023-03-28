263d4d05bece2b2a4944ce6b56e5828ebab8f379
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2023-03-28 16:38:24 +0200

    UI: Added some UI elements for validation
    - Added some front-end validation to show the user if they cannot craft a recipe using ngIf and ngClass directives

--- src/app/components/craft-block/craft-block.component.ts
+++ src/app/components/craft-block/craft-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-craft-block',
@@ -11,5 +11,7 @@ export class CraftBlockComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() canCraft: boolean;
 
 }
